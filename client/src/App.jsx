8670d087db3110471b09b23ca3f0206d32955006
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-02-08 10:05:01 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+// In a React component
+function App() {
+  return (
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Tailwind CSS 4!
+      </h1>
+  );
+}
+
+export default App;
